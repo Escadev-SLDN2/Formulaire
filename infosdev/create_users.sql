510ba144b04a43a881bc580fc67c14cd63922d22
Désir Thomas <thomasdesir.pro@gmail.com>
2020-02-05 16:57:01 +0100

    NOT NULL

--- infosdev/create_users.sql
+++ infosdev/create_users.sql
@@ -4,5 +4,5 @@ CREATE TABLE users (
     id int auto_increment primary key,
     name varchar(255) not null,
     email varchar(255) not null,
-    password varchar(255) not pull
+    password varchar(255) not null
 );
