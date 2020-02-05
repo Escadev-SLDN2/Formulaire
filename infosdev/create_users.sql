c94b1c7bbaf635cc4db480ca03c237abbff05c5c
Désir Thomas <thomasdesir.pro@gmail.com>
2020-02-05 16:54:48 +0100

    rajout de la colonne password dans la table users de la BDD formulaire

--- infosdev/create_users.sql
+++ infosdev/create_users.sql
@@ -3,14 +3,6 @@ USE formulaire;
 CREATE TABLE users (
     id int auto_increment primary key,
     name varchar(255) not null,
-    email varchar(255) not null
-);
-
-INSERT INTO users (
-	name, 
-    email
-) 
-VALUES (
-	"Bob Marley", 
-    "bob@marley.jm"
+    email varchar(255) not null,
+    password varchar(255) not pull
 );
